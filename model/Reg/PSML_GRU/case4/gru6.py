acfaec9e5d5227925ff632b5fb76085e8d8dae94
UmmeNabila <unabila>
2025-03-16 20:37:06 -0400

    gru

---
+++ model/Reg/PSML_GRU/case4/gru6.py
@@ -0,0 +1,189 @@
+import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import matplotlib.pyplot as plt
+import time
+import math
+import itertools
+
+# Start the timer
+start_time = time.time()
+
+# Define the sliding window function
+def sliding_window(x, y, lookback, lookforward):
+    x_ = []
+    y_ = []
+    y_gan = []
+    for i in range(lookback, x.shape[0] - lookforward + 1):
+        tmp_x = x[i - lookback: i, :]
+        tmp_y = y[i + lookforward - 1]
+        tmp_y_gan = y[i - lookback: i + lookforward]
+        x_.append(tmp_x)
+        y_.append(tmp_y)
+        y_gan.append(tmp_y_gan)
+    x_ = torch.from_numpy(np.array(x_)).float()
+    y_ = torch.from_numpy(np.array(y_)).float()
+    y_gan = torch.from_numpy(np.array(y_gan)).float()
+    return x_, y_, y_gan
+
+# Define the GRU model
+class GRUmodel(nn.Module):
+    def __init__(self, input_size):
+        super(GRUmodel, self).__init__()
+        self.gru = nn.GRU(input_size, 64, num_layers=2, batch_first=True, dropout=0.1)
+        self.linear_1 = nn.Linear(64, 3)
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, x):
+        x, _ = self.gru(x)
+        x = self.dropout(x[:, -1, :])
+        x = self.linear_1(x)
+        return x
+
+# Load data
+data = pd.read_csv("/home/unabila/wgan/CAISO_zone_1_.csv", index_col='time')
+
+
+# Move "wind power", "solar power", "load power" to the end of the DataFrame
+target_columns = ['wind_power', 'solar_power', 'load_power']
+other_columns = [col for col in data.columns if col not in target_columns]
+
+# Rearrange columns
+data = data[other_columns + target_columns]
+
+def smooth_dataset(df, window_size):
+    df_smoothed = df.groupby(np.arange(len(df)) // window_size).mean()
+    return df_smoothed
+
+window_size = 5
+data_smoothed = smooth_dataset(data, window_size)
+
+# Get the list of input features (assuming first 8 columns are inputs and last 3 are outputs)
+input_features = data_smoothed.columns[:-3]
+output_features = data_smoothed.columns[-3:]
+results = []
+
+# Generate and save new files with 2 input features excluded
+for feature_combination in itertools.combinations(input_features, 2):
+    # Create a new DataFrame without the current combination of 2 input features
+    new_data = data_smoothed.drop(columns=list(feature_combination))
+    
+    # Create a filename for the new file
+    new_file_name = f'{feature_combination[0]}_{feature_combination[1]}_removed.csv'
+
+
+    x = new_data.iloc[:, :6].values
+    y = new_data.iloc[:, 6:9].values  
+    
+    # Split data   
+    two_week = 1440 * 14 // window_size 
+    ten_days = 1440 * 10 // window_size
+    
+    train_x, test_x = x[:two_week, :], x[two_week:two_week + ten_days, :]
+    train_y, test_y = y[:two_week, :], y[two_week:two_week + ten_days, :]
+    
+    
+    # Scale data
+    x_scaler = MinMaxScaler(feature_range=(0, 1))
+    y_scaler = MinMaxScaler(feature_range=(0, 1))
+    
+    train_x = x_scaler.fit_transform(train_x)
+    test_x = x_scaler.transform(test_x)
+    
+    train_y = y_scaler.fit_transform(train_y)
+    test_y = y_scaler.transform(test_y)
+    
+    lookback = 144
+    lookforward = 144
+
+    # Prepare data with sliding window
+    train_x_slide, train_y_slide, _ = sliding_window(train_x, train_y, lookback, lookforward)
+    test_x_slide, test_y_slide, _ = sliding_window(test_x, test_y, lookback, lookforward)
+
+    # Move data to the device (GPU if available)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_x_slide = train_x_slide.to(device)
+    train_y_slide = train_y_slide.to(device)
+    test_x_slide = test_x_slide.to(device)
+
+    # Initialize the model, loss function, and optimizer
+    modelG = GRUmodel(input_size=train_x_slide.shape[2]).to(device)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(modelG.parameters(), lr=0.001)
+
+    # Training the model
+    num_epochs = 300
+    modelG.train()
+    for epoch in range(num_epochs):
+        optimizer.zero_grad()
+        output = modelG(train_x_slide)
+        loss = criterion(output, train_y_slide)
+        loss.backward()
+        optimizer.step()
+        if epoch % 10 == 0:
+            print(f'Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}')
+
+    # Evaluate the model
+    modelG.eval()
+    pred_y_test = modelG(test_x_slide)
+
+    y_test_true = y_scaler.inverse_transform(test_y_slide.cpu().numpy().reshape(-1, 3))
+    y_test_pred = y_scaler.inverse_transform(pred_y_test.cpu().detach().numpy())
+
+    pd.DataFrame(y_test_pred).to_csv(f'pred_test_data_gru_{feature_combination[0]}_{feature_combination[1]}_removed.csv', index=False)
+
+     # Calculate metrics and plot
+    metrics = {}
+    for i, param in enumerate(["wind power", "solar power", "load power"]):
+        MSE = mean_squared_error(y_test_true[:, i], y_test_pred[:, i])
+        RMSE = math.sqrt(MSE)
+        MAE = mean_absolute_error(y_test_true[:, i], y_test_pred[:, i])
+        R2 = r2_score(y_test_true[:, i], y_test_pred[:, i])
+    
+        # Sigma for real and generated data
+        real_sigma_last = np.std(y_test_true[:, i])
+        generated_sigma_last = np.std(y_test_pred[:, i])
+    
+        # Compute absolute differences
+        sigma = abs(real_sigma_last - generated_sigma_last) / real_sigma_last
+    
+        # Store results in dictionary
+        metrics[param] = {'RMSE': RMSE, 'MAE': MAE, 'R2': R2, 'Sigma': sigma}
+    
+        # Plot predictions
+        plt.figure(figsize=(12, 8))
+        plt.scatter(range(len(y_test_true[:, i])), y_test_true[:, i], color='black', label=f'Actual {param}', s=10)
+        plt.scatter(range(len(y_test_true[:, i])), y_test_pred[:, i], color='blue', label=f'Predict {param}', s=10)
+        plt.title(f'GAN prediction testing dataset ({param}) -{feature_combination[0]}_{feature_combination[1]}_removedmins')
+        plt.ylabel(param, fontsize=20)
+        plt.xlabel('Minutes', fontsize=20)
+        plt.legend(loc='upper right')
+        plt.savefig(f'prediction_gru_{feature_combination[0]}_{feature_combination[1]}_removed_{param.replace(" ", "_")}.png')
+    
+    # Append results for the current lookback
+    results.append({
+        'feature_to_remove': f'{feature_combination[0]}_{feature_combination[1]}',
+        'RMSE_wind': metrics["wind power"]['RMSE'],
+        'MAE_wind': metrics["wind power"]['MAE'],
+        'R2_wind': metrics["wind power"]['R2'],
+        'Sigma_wind': metrics["wind power"]['Sigma'],
+        'RMSE_solar': metrics["solar power"]['RMSE'],
+        'MAE_solar': metrics["solar power"]['MAE'],
+        'R2_solar': metrics["solar power"]['R2'],
+        'Sigma_solar': metrics["solar power"]['Sigma'],
+        'RMSE_load': metrics["load power"]['RMSE'],
+        'MAE_load': metrics["load power"]['MAE'],
+        'R2_load': metrics["load power"]['R2'],
+        'Sigma_load': metrics["load power"]['Sigma']
+    })
+
+# Convert results to DataFrame and save to CSV
+results_df = pd.DataFrame(results)
+results_df.to_csv('gru12h_2feature_to_remove_results.csv', index=False)
+
+# Print the elapsed time
+end_time = time.time()
+print(f"Total time elapsed: {(end_time - start_time) / 60:.2f} minutes")
---
+++ model/cGAN/CHF/case3/3fix/mt/cd3gan.py
@@ -0,0 +1,214 @@
+# 3,3
+from keras.models import Model
+from keras.layers import Input, Dense, Concatenate
+from keras.optimizers import Adam
+import numpy as np
+import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+from itertools import product
+
+def mean_absolute_percentage_error(y_true, y_pred):
+    import numpy as np
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+# define the standalone discriminator model
+def build_generator(z_dim, x_dim, num_layers, num_nodes):
+    z = Input(shape=(z_dim,))
+    x = Input(shape=(x_dim,))
+
+    merged_layer = Concatenate()([z, x])
+    hidden = Dense(num_nodes, activation='relu')(merged_layer)
+    for i in range(1, num_layers):
+        hidden = Dense(num_nodes, activation='relu')(hidden)
+    out_layer = Dense(3, activation='linear')(hidden)  # Output layer with 3 neurons for 3 output columns
+
+    model = Model(inputs=[z, x], outputs=out_layer)
+    return model
+
+# Define the discriminator model
+def build_discriminator(x_dim, learning_rate, num_layers, num_nodes):
+    x = Input(shape=(x_dim + 3,))  # Adjusted shape to include 3 output columns
+
+    hidden = Dense(num_nodes, activation='relu')(x)
+    for i in range(1, num_layers):
+        hidden = Dense(num_nodes, activation='relu')(hidden)
+    out_layer = Dense(1, activation='sigmoid')(hidden)
+
+    model = Model(inputs=x, outputs=out_layer)
+
+    model.compile(loss='binary_crossentropy', optimizer=Adam(lr=learning_rate), metrics=['mean_absolute_error'])
+    return model
+
+def build_cgan(generator, discriminator):
+    z_dim = generator.input_shape[0][1]
+    x_dim = generator.input_shape[1][1]
+
+    discriminator.trainable = False
+
+    z = Input(shape=(z_dim,))
+    x = Input(shape=(x_dim,))
+    generated_y = generator([z, x])
+
+    cgan_output = discriminator(Concatenate()([x, generated_y]))
+
+    cgan = Model(inputs=[z, x], outputs=cgan_output)
+    cgan.compile(loss='binary_crossentropy', optimizer=Adam())
+
+    return cgan
+
+def train_cgan(generator, discriminator, cgan, x_train, y_train, epochs, z_dim, batch_size):
+
+    valid = np.ones((batch_size, 1))
+    fake = np.zeros((batch_size, 1))
+
+    for epoch in range(epochs):
+
+        # Random indices for a batch
+        idx = np.random.randint(0, x_train.shape[0], batch_size)
+        true_x, true_y = x_train[idx], y_train[idx]
+
+        # Train the discriminator with real samples
+        d_loss_real = discriminator.train_on_batch(np.hstack([true_x, true_y]), valid)
+
+        # Generate fake samples
+        noise = np.random.normal(0, 1, (batch_size, z_dim))
+        gen_y = generator.predict([noise, true_x])
+
+        # Train the discriminator with fake samples
+        d_loss_fake = discriminator.train_on_batch(np.hstack([true_x, gen_y]), fake)
+
+        # Train the generator (through combined cGAN model)
+        g_loss = cgan.train_on_batch([noise, true_x], valid)
+
+        if epoch % 100 == 0:
+            print(f"{epoch} [D loss: {np.mean([d_loss_real[0], d_loss_fake[0]])}] [G loss: {g_loss}]")
+
+def generate_y_test(generator, x_test, z_dim):
+    noise = np.random.normal(0, 1, (x_test.shape[0], z_dim))
+    generated_y_test = generator.predict([noise, x_test])
+    # Assuming generator outputs three values, keep all rows and select only the last three columns
+    return generated_y_test[:, -3:]
+
+def plot_comparison(df1,df2, num_points_from_file2,item):
+		# Convert numpy arrays to pandas DataFrames
+    df1 = pd.DataFrame(df1)
+    df2 = pd.DataFrame(df2)
+
+    # Take the same number of data points randomly from the first file as from the second file
+    df1_sampled = df1.sample(n=num_points_from_file2, random_state=42)
+
+    # Get the number of columns (excluding the last column)
+    num_cols = len(df1_sampled.columns) - 1
+    header = ["diameter","legth","Pressure","Mass flow rate","Temperature","CHF"]
+
+    # Create subplots
+    fig, axes = plt.subplots(1, num_cols, figsize=(5*num_cols, 5))
+
+    # Iterate through each column (excluding the last column)
+    for i in range(num_cols):
+        # Scatter plot for each file's column against the last column
+        axes[i].scatter(df1_sampled.iloc[:, i], df1_sampled.iloc[:, -1], color='red', label='Real')
+        axes[i].scatter(df2.iloc[:, i], df2.iloc[:, -1], color='blue', label='Fake')
+
+        # Customize each subplot
+        axes[i].set_title(f'{header[i]} vs CHF')
+        axes[i].set_xlabel(f'{header[i]}')
+        axes[i].set_ylabel('CHF')
+        axes[i].legend()
+
+    # Customize the overall plot layout
+    plt.tight_layout()
+
+    # Save the plot
+    plt.savefig(f'comparison_plot_MS_{item}.png')
+
+#######################################################################################################
+# Define configurations
+learning_rate = [5e-4]
+num_layers = [4]
+num_nodes = [200]
+batch_size = [128]
+configs = list(product(learning_rate, num_layers, num_nodes,batch_size))
+MS_list=[]
+
+# Create an empty DataFrame to store results
+gridres = pd.DataFrame(columns=['run', 'learning_rate', 'num_layers', 'num_nodes', 'Mae', 'Mse', 'R2'])
+# Assuming x_train are your features and y_train are the labels you want to generate.
+# Load real_data and input_data from CSV files
+# Read train data
+train_data = pd.read_csv("/home/unabila/CondGan/3fix/train_m_t.csv").values
+scaler = MinMaxScaler()
+train_data = scaler.fit_transform(train_data)
+xtrain = train_data[:, :3]
+ytrain = train_data[:, -3:]
+
+# Read test data
+test_data = pd.read_csv("/home/unabila/CondGan/3fix/test_m_t.csv").values
+Test = test_data
+Ytest = test_data[:, -1]
+test_data = scaler.transform(test_data)  # Use the same scaler as for train data
+xtest = test_data[:, :3]
+ytest = test_data[:, -3:]
+
+# Dimensions
+z_dim = 100  # Latent vector size
+x_dim = xtrain.shape[1]  # Feature size
+for item in configs:
+    # create the discriminator
+    discriminator = build_discriminator(x_dim,learning_rate= item[0], num_layers= item[1], num_nodes= item[2])
+    # create the generator
+    generator = build_generator(z_dim, x_dim, num_layers= item[1], num_nodes= item[2])
+    # create the gan
+    cgan = build_cgan(generator, discriminator)
+    # train model(g_model, d_model, gan_model, real_input_data, latent_dim, learning_rate, num_layers, num_nodes,..)
+    epochs = 6000   # Number of epochs you want to train for
+    train_cgan(generator, discriminator, cgan, xtrain, ytrain, epochs, z_dim, batch_size = item[3] )
+
+    # generate data
+    Ynew = generate_y_test(generator, xtest, z_dim)
+
+    # Concatenate generated output with input_data along the columns axis
+    generated_data = np.concatenate((xtest, Ynew), axis=1)
+    generated_data = scaler.inverse_transform(generated_data)
+    Ynn = generated_data[:, -1]
+
+    mape = mean_absolute_percentage_error(Ytest, Ynn)
+    mse = mean_squared_error(Ytest, Ynn)
+    r2 = r2_score(Ytest, Ynn)
+    MS_list.append((mape,mse,r2))
+
+    # Save generated data to a CSV file
+    columns = ['Diameter', 'length', 'Pressure', 'Mass flux', 'Temperature', 'CHF']
+    generated_df = pd.DataFrame(generated_data, columns=columns)
+    generated_df.to_csv(f'generated_samples_{item}.csv', index=False)
+    #plot_comparison(Test,generated_data,2100,item) #dont forget to change
+
+
+gridres = pd.DataFrame(configs, columns = ['learning_rate', 'num_layers', 'num_nodes','batch_size'])
+mean_values = [item[0] for item in MS_list]
+mse_values = [item[1] for item in MS_list]
+r2 = [item[2] for item in MS_list]
+
+# Create a DataFrame for the results
+results_df = pd.DataFrame(MS_list, columns=['MAPE', 'MSE', 'R2'])
+
+# Concatenate gridres and results_df along the columns axis
+final_result = pd.concat([gridres, results_df], axis=1)
+
+# Save the results to a CSV file
+final_result.to_csv('resultsLr.csv', index=False)
+
+# Assign mean and sigma values to separate columns in the DataFrame
+gridres['Mape'] = mean_values
+gridres['Mse'] = mse_values
+gridres['R2'] = r2
+
+# Sort the DataFrame by 'Error' column in ascending order
+gridres = gridres.sort_values(by='R2', ascending=False)
+
+# Print the first few rows of the sorted DataFrame
+print(gridres.head())
