2f7fb6e69fb3410e94e46c0d146907d3d7388be6
UmmeNabila <143683605+UmmeNabila@users.noreply.github.com>
2025-03-17 12:24:50 -0400

    Update ff1k.py

--- model/Reg/CHF_FNN/case1/1k/ff1k.py
+++ model/Reg/CHF_FNN/case1/1k/ff1k.py
@@ -57,13 +57,13 @@ configs = list(product(learning_rate, num_layers, num_nodes, batch_size))
 MS_list = []
 
 # Load and preprocess data
-train_data = pd.read_csv("/home/unabila/CondGan/1k/train_1k.csv").values
+train_data = pd.read_csv("train_1k.csv").values
 scaler = MinMaxScaler()
 train_data = scaler.fit_transform(train_data)
 x_train = train_data[:, :-1]
 y_train = train_data[:, -1]
 
-test_data = pd.read_csv("/home/unabila/CondGan/1k/test_all.csv").values
+test_data = pd.read_csv("test_all.csv").values
 Y_test = test_data[:, -1]
 test_data = scaler.transform(test_data)
 x_test = test_data[:, :-1]
